d8dcfd484b539502e4d4ce99f6a42a1d07b305a1
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-10-03 18:31:49 -0400

    added serializers.py and updated models/views

--- marvelApp/models.py
+++ marvelApp/models.py
@@ -4,5 +4,3 @@ class Movie(models.Model):
     title         = models.CharField(max_length=30)
     year          = models.DateField()
     fav_character = models.CharField(max_length=30)
-    movie         = models.CharField(max_length=30)
-    new_character = models.CharField(max_length=30)
---
+++ marvelApp/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Movie
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Movie
+        fields = [
+            'id', 
+            'title', 
+            'year', 
+            'fav_character'
+            ]
