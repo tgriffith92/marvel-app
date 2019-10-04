0c93c0264689d6045d847acb5ed747dda74c68f9
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-10-04 10:16:38 -0400

    add the comic model

--- marvelApp/models.py
+++ marvelApp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 class Movie(models.Model):
-    title              = models.CharField(max_length=50)
-    year               = models.DateField()
+    title = models.CharField(max_length=50)
+    year  = models.DateField()
+
+class Comic(models.Model):
+    title  = models.CharField(max_length=50)
+    rating = models.PositiveSmallIntegerField()
+    review = models.TextField()
