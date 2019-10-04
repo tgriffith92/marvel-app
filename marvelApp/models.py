0b94b6c6bd675196cdb95c2e0d9f97fa10a8eaa8
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-10-04 11:00:04 -0400

    added the character model

--- marvelApp/models.py
+++ marvelApp/models.py
@@ -8,3 +8,9 @@ class Comic(models.Model):
     title  = models.CharField(max_length=50)
     rating = models.PositiveSmallIntegerField()
     review = models.TextField()
+
+class Character(models.Model):
+    name        = models.CharField(max_length=30)
+    ability     = models.PositiveSmallIntegerField()
+    affiliation = models.CharField(max_length=10)
+    reason      = models.TextField()
