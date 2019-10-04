bdb7ecc1452aa6e17a88eb58c79370ef82af2624
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-10-04 15:12:02 -0400

    add the new character form and update the state

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class NewCharacterForm extends React.Component {
+
+  state = {
+    name: '',
+    affiliation: '',
+    reason: ''
+  }
+
+  handleInput = (evnt) => {
+    let newCharacter = {...this.state}
+
+    newCharacter[evnt.target.name] = evnt.target.value
+
+    this.setState(newCharacter)
+  }
+
+  handleSubmit = (evnt) => {
+    evnt.preventDefault();
+
+    this.props.addNewChar(this.state)
+    this.setState({name: '', affiliation: '', reason: ''})
+  }
+
+  render = () => (
+    <form onSubmit={this.handleSubmit}>
+      <label htmlFor='name'>Character Name:</label><br />
+      <input type='text' name='name' onChange={this.handleInput} placeholder='Namor'/><br />
+      <label htmlFor='affiliation'>Affiliation:</label><br />
+      <select>
+        <option value=''>Hero</option>
+        <option value=''>Villain</option>
+      </select><br />
+      <label htmlFor='reason'>Reason:</label><br />
+      <textarea rows='4' cols='50' name='reason' onChange={this.handleInput}>Enter text here...</textarea><br />
+      <input type='submit' value='New Character'/>
+    </form>
+  )
+}
+
+class App extends React.Component {
+  render = () => (
+    <div>
+      <NewCharacterForm />
     </div>
-  );
+  )
 }
 
 export default App;
