9ffd8cd4a6336c103a2b55b20d8f02bae24b27f1
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-10-04 10:20:13 -0400

    added the comic serializer and viewset

--- marvelApp/serializers.py
+++ marvelApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Comic
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,4 +8,14 @@ class MovieSerializer(serializers.ModelSerializer):
             'id', 
             'title', 
             'year'
+            ]
+
+class ComicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Comic
+        fields = [
+            'id', 
+            'title', 
+            'rating',
+            'review'
             ]
--- marvelApp/views.py
+++ marvelApp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import MovieSerializer
-from .models import Movie
+from .serializers import MovieSerializer, ComicSerializer
+from .models import Movie, Comic
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset         = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class ComicViewSet(viewsets.ModelViewSet):
+    queryset         = Comic.objects.all()
+    serializer_class = ComicSerializer
