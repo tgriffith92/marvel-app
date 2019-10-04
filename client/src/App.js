63d9f9e027c1f335931a2d1c559789cf5c7579b8
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-10-04 15:53:55 -0400

    add the comic form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import './App.css';
 
+class NewComicForm extends React.Component {
+  state = {
+    title: '',
+    rating: 1,
+    review: ''
+  }
+
+  handleInput = (evnt) => {
+    let newComic = {...this.state}
+
+    newComic[evnt.target.name] = evnt.target.value
+
+    this.setState(newComic)
+  }
+
+  
+
+  render = () => (
+    <form>
+      <label htmlFor='title'>Title:</label><br />
+      <input type='text' name='title' onChange={this.handleInput} placeholder='Civil War'/><br />
+      <label htmlFor='rating'>Rating(1-5):</label><br />
+      <input type='number' name='rating' min='1' max='5' onChange={this.handleInput}/>
+      <label htmlFor='reason'>Reason:</label><br />
+      <textarea rows='4' cols='50' name='reason' onChange={this.handleInput}>Enter text here...</textarea><br />
+      <input type='submit' value='New Character'/>
+    </form>
+  )
+}
+
 class NewCharacterForm extends React.Component {
 
   state = {
@@ -28,8 +58,16 @@ class NewCharacterForm extends React.Component {
     this.setState({name: '', affiliation: '', reason: ''})
   }
 
+  // addNewChar = (newCharInfo) => {
+  //   saveCharToServer(newCharInfo)
+  //     .then(newChar => {
+  //       console.log(newChar);
+  //       newChar
+  //     })
+  // }
+
   render = () => (
-    <form onSubmit={this.handleSubmit}>
+    <form>
       <label htmlFor='name'>Character Name:</label><br />
       <input type='text' name='name' onChange={this.handleInput} placeholder='Namor'/><br />
       <label htmlFor='affiliation'>Affiliation:</label><br />
@@ -48,6 +86,7 @@ class App extends React.Component {
   render = () => (
     <div>
       <NewCharacterForm />
+      <NewComicForm />
     </div>
   )
 }
